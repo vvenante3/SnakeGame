1283f47684f6d718e5e24d4462b0a381df394fc6
vvenante3 <vitor.venante@hotmail.com>
2023-10-10 19:44:11 -0300

    inicialização JavaScript

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("canvas");
+
+// ctx = contexto
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(280, 280, 50, 50); //X, Y, largura e altura
